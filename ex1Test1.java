68510bf44057e5a642eab41dd6627b3642b5e8d0
mabingqian <dream23qian@163.com>
2017-04-13 22:20:43 +0800

    Update ex1Test1.java

--- ex1Test1.java
+++ ex1Test1.java
@@ -3,7 +3,7 @@ public class ex1Test1{
 	public static void main(String[] args){
 		System.out.println("hello world");
 	}
-
+	/* it is a say() hanshu */
 	public void say(){
 		System.out.println();
 	}
